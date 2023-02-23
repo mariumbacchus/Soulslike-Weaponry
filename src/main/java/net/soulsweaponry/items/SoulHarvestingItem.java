502e78a86ad6d4455e9845328cd02414884d19f0
mariuj0909 <mariuj0909@osloskolen.no>
2023-02-23 17:18:14 +0100

    Bosses grant 50 souls for SoulHarvestingItem, Verglas y -63

--- src/main/java/net/soulsweaponry/items/SoulHarvestingItem.java
+++ src/main/java/net/soulsweaponry/items/SoulHarvestingItem.java
@@ -1,9 +1,11 @@
 package net.soulsweaponry.items;
 
+import net.minecraft.entity.boss.WitherEntity;
 import net.minecraft.item.SwordItem;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolMaterial;
+import net.soulsweaponry.entity.mobs.BossEntity;
 
 public class SoulHarvestingItem extends SwordItem {
 
@@ -17,7 +19,11 @@ public class SoulHarvestingItem extends SwordItem {
     public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         super.postHit(stack, target, attacker);
         if (target.isDead()) {
-            this.addKillCounter(stack);
+            if (target instanceof BossEntity || target instanceof WitherEntity) {
+                this.addAmount(stack, 50);
+            } else {
+                this.addKillCounter(stack);
+            }
         }
         return true;
     }
