5ddf890b90473ab3b53de829e29795846b185e7f
mariuj0909 <mariuj0909@osloskolen.no>
2023-03-14 21:46:04 +0100

    Added pentagram particles to Altar Block and bonus damage to Sting

--- src/main/java/net/soulsweaponry/mixin/EnchantmentHelperMixin.java
+++ src/main/java/net/soulsweaponry/mixin/EnchantmentHelperMixin.java
@@ -1,7 +1,11 @@
 package net.soulsweaponry.mixin;
 
+import net.minecraft.entity.EntityGroup;
+import net.soulsweaponry.config.ConfigConstructor;
+import net.soulsweaponry.registry.WeaponRegistry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -11,11 +15,12 @@ import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.soulsweaponry.items.GunItem;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EnchantmentHelper.class)
 public class EnchantmentHelperMixin {
     
-    @Unique
+    /*@Unique
     private static Enchantment currentEnchantment;
 
     @Redirect(method = "getPossibleEntries", at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/EnchantmentTarget;isAcceptableItem(Lnet/minecraft/item/Item;)Z"))
@@ -27,5 +32,13 @@ public class EnchantmentHelperMixin {
         }
 
         return enchantmentTarget.isAcceptableItem(item);
+    }*/
+
+    @Inject(method = "getAttackDamage", at = @At("TAIL"), cancellable = true)
+    private static void interceptGetDamage(ItemStack stack, EntityGroup group, CallbackInfoReturnable<Float> cir) {
+        if (stack.isOf(WeaponRegistry.STING) && group == EntityGroup.ARTHROPOD) {
+            float value = cir.getReturnValue() + ConfigConstructor.sting_bonus_arthropod_damage;
+            cir.setReturnValue(value);
+        }
     }
 }
