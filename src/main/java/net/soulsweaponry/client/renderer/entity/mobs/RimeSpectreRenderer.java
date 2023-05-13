efc594233ea3a5d9aa619abc538bb68a01271617
mariuj0909 <mariuj0909@osloskolen.no>
2023-05-13 18:02:16 +0200

    Added RimeSpectre

---
+++ src/main/java/net/soulsweaponry/client/renderer/entity/mobs/RimeSpectreRenderer.java
@@ -0,0 +1,27 @@
+package net.soulsweaponry.client.renderer.entity.mobs;
+
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.util.Identifier;
+import net.soulsweaponry.client.model.entity.mobs.RimeSpectreModel;
+import net.soulsweaponry.entity.mobs.RimeSpectre;
+import software.bernie.geckolib.renderer.GeoEntityRenderer;
+
+public class RimeSpectreRenderer extends GeoEntityRenderer<RimeSpectre> {
+
+    public RimeSpectreRenderer(EntityRendererFactory.Context ctx) {
+        super(ctx, new RimeSpectreModel());
+        this.shadowRadius = 0.7F;
+    }
+
+    @Override
+    protected float getDeathMaxRotation(RimeSpectre entityLivingBaseIn) {
+        return 0f;
+    }
+
+    @Override
+    public RenderLayer getRenderType(RimeSpectre animatable, Identifier texture, VertexConsumerProvider bufferSource, float partialTick) {
+        return RenderLayer.getEntityTranslucent(this.getTexture(animatable));
+    }
+}
