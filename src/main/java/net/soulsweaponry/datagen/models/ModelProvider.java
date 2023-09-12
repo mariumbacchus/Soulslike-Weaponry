bf32cf7de8798437e29795d3dd57fd3dabbf3ac6
mariuj0909 <mariuj0909@osloskolen.no>
2023-09-12 18:06:31 +0200

    Fixed bug where game crashes with Valhelsia Core Due to Valhelsia Core redirecting a mixin used by fabric's ModelProvider, the game crashes if this mod tries to utilize the model generation. Deprecated this mod's uses of those model generation classes.

--- src/main/java/net/soulsweaponry/datagen/models/ModelProvider.java
+++ src/main/java/net/soulsweaponry/datagen/models/ModelProvider.java
@@ -10,6 +10,9 @@ import net.soulsweaponry.mixin.ModelInvoker;
 
 import java.util.HashMap;
 
+// Due to bug regarding Valhelsia Core redirecting the mixin used in the model provider, the game crashes if both this
+// mod and VC is installed, therefore, the generation of models here has been deprecated.
+@Deprecated
 public class ModelProvider extends FabricModelProvider {
 
     public static HashMap<Item, Model> ITEMS = new HashMap<>();
