e8d6a881375093c78ef442549d3a1a60c0d61cdc
mariuj0909 <mariuj0909@osloskolen.no>
2023-09-14 12:51:20 +0200

    Added NightProwler base class

--- src/main/java/net/soulsweaponry/entity/mobs/DayStalker.java
+++ src/main/java/net/soulsweaponry/entity/mobs/DayStalker.java
@@ -506,7 +506,7 @@ public class DayStalker extends BossEntity implements GeoEntity {
         if (this.getAttackAnimation().equals(Attacks.OVERHEAT)) {
             amount = amount * 0.6f;
         }
-        if (source.isIn(DamageTypeTags.IS_PROJECTILE)) {
+        if (source.isIn(DamageTypeTags.IS_PROJECTILE) && !this.isFlying()) {
             amount = amount * 0.75f;
         }
         return super.damage(source, amount);
