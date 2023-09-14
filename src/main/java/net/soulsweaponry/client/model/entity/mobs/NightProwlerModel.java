63e50e6e4d8ff93769c5fb2b391028faf34eee36
marium <mariummailtest@gmail.com>
2023-09-14 15:08:14 +0200

    Rotates head

--- src/main/java/net/soulsweaponry/client/model/entity/mobs/NightProwlerModel.java
+++ src/main/java/net/soulsweaponry/client/model/entity/mobs/NightProwlerModel.java
@@ -3,7 +3,10 @@ package net.soulsweaponry.client.model.entity.mobs;
 import net.minecraft.util.Identifier;
 import net.soulsweaponry.SoulsWeaponry;
 import net.soulsweaponry.entity.mobs.NightProwler;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class NightProwlerModel extends AnimatedGeoModel<NightProwler> {
 
@@ -20,5 +23,18 @@ public class NightProwlerModel extends AnimatedGeoModel<NightProwler> {
     @Override
     public Identifier getAnimationFileLocation(NightProwler object) {
         return new Identifier(SoulsWeaponry.ModId, "animations/night_prowler.animation.json");
+    }
+
+    @SuppressWarnings({"unchecked"})
+    @Override
+    public void setCustomAnimations(NightProwler entity, int uniqueID, AnimationEvent customPredicate) {
+        super.setCustomAnimations(entity, uniqueID, customPredicate);
+        IBone head = this.getAnimationProcessor().getBone("head");
+
+        EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+        if (head != null && !entity.isDead()) {
+            head.setRotationX(extraData.headPitch * ((float) Math.PI / 180F));
+            head.setRotationY(extraData.netHeadYaw * ((float) Math.PI / 180F));
+        }
     }
 }
