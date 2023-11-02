e705fe1b8ad20ab2c7ba105e5c6059f0dd63a361
marium <mariummailtest@gmail.com>
2023-11-02 18:13:40 +0100

    Added MoonlightArrow

--- src/main/java/net/soulsweaponry/registry/EntityRegistry.java
+++ src/main/java/net/soulsweaponry/registry/EntityRegistry.java
@@ -73,6 +73,7 @@ public class EntityRegistry {
     public static final EntityType<NightWaveEntity> NIGHT_WAVE = Registry.register(Registries.ENTITY_TYPE, new Identifier(ModId, "night_wave"), FabricEntityTypeBuilder.create(SpawnGroup.MISC, NightWaveEntity::new).dimensions(EntityDimensions.fixed(3.5f, 1f)).fireImmune().trackRangeChunks(4).trackedUpdateRate(Integer.MAX_VALUE).build());
     public static final EntityType<FlamePillar> FLAME_PILLAR = registerEntity("flame_pillar", FabricEntityTypeBuilder.create(SpawnGroup.MISC, FlamePillar::new).dimensions(EntityDimensions.fixed(1.5f, 1.5f)).fireImmune().trackRangeChunks(4).trackedUpdateRate(Integer.MAX_VALUE).build());
     public static final EntityType<KrakenSlayerProjectile> KRAKEN_SLAYER_PROJECTILE = registerEntity("kraken_slayer_projectile", FabricEntityTypeBuilder.create(SpawnGroup.MISC, KrakenSlayerProjectile::new).dimensions(EntityDimensions.fixed(0.5f, 0.5f)).trackRangeChunks(4).trackedUpdateRate(20).build());
+    public static final EntityType<MoonlightArrow> MOONLIGHT_ARROW = registerEntity("moonlight_arrow", FabricEntityTypeBuilder.create(SpawnGroup.MISC, MoonlightArrow::new).dimensions(EntityDimensions.fixed(0.5f, 0.5f)).trackRangeChunks(4).trackedUpdateRate(20).build());
 
     public static final Item WITHERED_DEMON_SPAWN_EGG = new SpawnEggItem(WITHERED_DEMON, 10027008, 0, new FabricItemSettings());
     public static final Item ACCURSED_LORD_BOSS_SPAWN_EGG = new SpawnEggItem(ACCURSED_LORD_BOSS, 0, 10027008, new FabricItemSettings());
