b19bb1ed271a63ed49530972d3e496563c5abc62
mariuj0909 <mariuj0909@osloskolen.no>
2023-12-05 19:06:36 +0100

    Fixed SkofnungStone compatibility

--- src/main/java/net/soulsweaponry/items/SkofnungStone.java
+++ src/main/java/net/soulsweaponry/items/SkofnungStone.java
@@ -3,6 +3,7 @@ package net.soulsweaponry.items;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -12,6 +13,7 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 import net.soulsweaponry.config.ConfigConstructor;
 import net.soulsweaponry.registry.EffectRegistry;
@@ -39,8 +41,8 @@ public class SkofnungStone extends Item {
                 world.playSound(user, user.getBlockPos(), SoundEvents.ENTITY_ENDER_DRAGON_GROWL, SoundCategory.PLAYERS, .5f, .5f);
             }
         }
-        for (StatusEffect effect : this.harmfulEffects()) {
-            if (user.hasStatusEffect(effect)) {
+        for (StatusEffect effect : Registry.STATUS_EFFECT) {
+            if (effect.getCategory().equals(StatusEffectCategory.HARMFUL) && user.hasStatusEffect(effect)) {
                 user.removeStatusEffect(effect);
                 world.playSound(user, user.getBlockPos(), SoundRegistry.RESTORE_EVENT, SoundCategory.PLAYERS, 1f, 1f);
                 shouldDamage = true;
@@ -63,30 +65,5 @@ public class SkofnungStone extends Item {
             tooltip.add(Text.translatable("tooltip.soulsweapons.shift"));
         }
         super.appendTooltip(stack, world, tooltip, context);
-    }
-
-    public StatusEffect[] harmfulEffects() {
-        return new StatusEffect[]{
-            StatusEffects.BLINDNESS,
-            StatusEffects.DARKNESS,
-            StatusEffects.HUNGER,
-            StatusEffects.INSTANT_DAMAGE,
-            StatusEffects.LEVITATION,
-            StatusEffects.MINING_FATIGUE,
-            StatusEffects.NAUSEA,
-            StatusEffects.POISON,
-            StatusEffects.SLOWNESS,
-            StatusEffects.UNLUCK,
-            StatusEffects.WEAKNESS,
-            StatusEffects.WITHER,
-            EffectRegistry.POSTURE_BREAK,
-            EffectRegistry.BLEED,
-            EffectRegistry.DISABLE_HEAL,
-            EffectRegistry.FEAR,
-            EffectRegistry.FREEZING,
-            EffectRegistry.DECAY,
-            EffectRegistry.RETRIBUTION,
-            EffectRegistry.BLIGHT,
-        };
     }
 }
