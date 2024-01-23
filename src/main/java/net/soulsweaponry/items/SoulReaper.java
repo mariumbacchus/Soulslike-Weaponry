bf80c9f3c3874e555b44fd56b08b65de0efec9a3
mariuj0909 <mariuj0909@osloskolen.no>
2024-01-23 22:51:16 +0100

    Removed unused method

--- src/main/java/net/soulsweaponry/items/SoulReaper.java
+++ src/main/java/net/soulsweaponry/items/SoulReaper.java
@@ -1,17 +1,11 @@
 package net.soulsweaponry.items;
 
-import java.util.List;
-import java.util.Random;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
-
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.client.render.item.BuiltinModelItemRenderer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolMaterial;
-import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.text.Text;
@@ -25,19 +19,21 @@ import net.soulsweaponry.config.ConfigConstructor;
 import net.soulsweaponry.entity.mobs.Forlorn;
 import net.soulsweaponry.entity.mobs.SoulReaperGhost;
 import net.soulsweaponry.entity.mobs.Soulmass;
-import net.soulsweaponry.registry.EntityRegistry;
 import net.soulsweaponry.networking.PacketRegistry;
+import net.soulsweaponry.registry.EntityRegistry;
 import net.soulsweaponry.registry.SoundRegistry;
 import net.soulsweaponry.util.ParticleNetworking;
 import net.soulsweaponry.util.WeaponUtil;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.animatable.client.RenderProvider;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
-import software.bernie.geckolib.core.animation.AnimatableManager;
-import software.bernie.geckolib.core.animation.AnimationController;
+import software.bernie.geckolib.core.animation.*;
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
-import software.bernie.geckolib.core.animation.*;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class SoulReaper extends SoulHarvestingItem implements GeoItem {
 
@@ -58,8 +54,6 @@ public class SoulReaper extends SoulHarvestingItem implements GeoItem {
                 Vec3d vecBlocksAway = player.getRotationVector().multiply(3).add(player.getPos());
                 if (!world.isClient) {
                     ParticleNetworking.sendServerParticlePacket((ServerWorld) world, PacketRegistry.CONJURE_ENTITY_PACKET_ID, new BlockPos(vecBlocksAway), 50);
-                } else {
-                    this.spawnParticles(world, vecBlocksAway.x, player.getY() + .1f, vecBlocksAway.z);
                 }
 
                 world.playSound(player, player.getBlockPos(), SoundRegistry.NIGHTFALL_SPAWN_EVENT, SoundCategory.PLAYERS, 0.8f, 1f);
@@ -92,19 +86,6 @@ public class SoulReaper extends SoulHarvestingItem implements GeoItem {
         return TypedActionResult.fail(stack); 
 	}
 
-    private void spawnParticles(World world, double x, double y, double z) {
-        Random random = new Random();
-        double d = random.nextGaussian() * 0.05D;
-        double e = random.nextGaussian() * 0.05D;
-        for(int i = 0; i < 50; ++i) {
-            double newX = random.nextDouble() - 0.5D + random.nextGaussian() * 0.15D + d;
-            double newZ = random.nextDouble() - 0.5D + random.nextGaussian() * 0.15D + e;
-            double newY = random.nextDouble() - 0.5D + random.nextDouble() * 0.5D;
-            world.addParticle(ParticleTypes.SOUL, x, y, z, newX/8, newY/2, newZ/8);
-            world.addParticle(ParticleTypes.DRAGON_BREATH, x, y, z, newX/8, newY/2, newZ/8);
-        }
-    }
-
     @Override
     public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
         if (Screen.hasShiftDown()) {
@@ -117,7 +98,7 @@ public class SoulReaper extends SoulHarvestingItem implements GeoItem {
         super.appendTooltip(stack, world, tooltip, context);
     }
 
-    private PlayState predicate(AnimationState event){
+    private PlayState predicate(AnimationState<?> event){
         event.getController().setAnimation(RawAnimation.begin().then("low_souls", Animation.LoopType.LOOP));
         return PlayState.CONTINUE;
     }
