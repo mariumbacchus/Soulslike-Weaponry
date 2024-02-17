a96833d5e138d63574d9445bc53c5d2aa136041b
marium <mariummailtest@gmail.com>
2024-02-17 20:56:53 +0100

    Buffed ChaosSet armor and toughness values Changed chaos_armor.png texture Added effect flip ability to crown of chaos

--- src/main/java/net/soulsweaponry/items/ChaosSet.java
+++ src/main/java/net/soulsweaponry/items/ChaosSet.java
@@ -1,5 +1,6 @@
 package net.soulsweaponry.items;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.function.Consumer;
@@ -15,6 +16,8 @@ import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
@@ -34,6 +37,7 @@ import net.soulsweaponry.blocks.WitheredTallFlower;
 import net.soulsweaponry.blocks.WitheredTallGrass;
 import net.soulsweaponry.client.renderer.armor.ChaosArmorRenderer;
 import net.soulsweaponry.client.renderer.armor.ChaosSetRenderer;
+import net.soulsweaponry.config.ConfigConstructor;
 import net.soulsweaponry.registry.BlockRegistry;
 import net.soulsweaponry.registry.ItemRegistry;
 import org.jetbrains.annotations.Nullable;
@@ -52,6 +56,10 @@ public class ChaosSet extends ArmorItem implements GeoItem {
     private final HashMap<Block, WitheredBlock> turnableBlocks = new HashMap<>();
     private final HashMap<Block, WitheredGrass> turnableGrass = new HashMap<>();
     private final HashMap<Block, WitheredTallGrass> turnableTallPlant = new HashMap<>();
+    /**
+     * Will contain harmful effects as the key, and the opposite beneficial effect as value
+     */
+    private static final HashMap<StatusEffect, StatusEffect> flippableEffects = new HashMap<>();
     
     public ChaosSet(ArmorMaterial material, EquipmentSlot slot, Settings settings) {
         super(material, slot, settings);
@@ -73,6 +81,9 @@ public class ChaosSet extends ArmorItem implements GeoItem {
         if (entity instanceof PlayerEntity player) {
             if (this.isHelmetEquipped(player)) {
                 player.addStatusEffect(new StatusEffectInstance(StatusEffects.LUCK, 40, 0));
+                if (!player.getItemCooldownManager().isCoolingDown(ItemRegistry.CHAOS_CROWN) && !player.getItemCooldownManager().isCoolingDown(ItemRegistry.CHAOS_HELMET)) {
+                    this.flipEffects(player);
+                }
             }
             if (this.isRobesEquipped(player)) {
                 this.turnBlocks(player, world, player.getBlockPos(), 0);
@@ -125,6 +136,45 @@ public class ChaosSet extends ArmorItem implements GeoItem {
         }
     }
 
+    static {
+        flippableEffects.put(StatusEffects.SLOWNESS, StatusEffects.SPEED);
+        flippableEffects.put(StatusEffects.MINING_FATIGUE, StatusEffects.HASTE);
+        flippableEffects.put(StatusEffects.WEAKNESS, StatusEffects.STRENGTH);
+        flippableEffects.put(StatusEffects.BLINDNESS, StatusEffects.NIGHT_VISION);
+        flippableEffects.put(StatusEffects.HUNGER, StatusEffects.SATURATION);
+        flippableEffects.put(StatusEffects.LEVITATION, StatusEffects.SLOW_FALLING);
+    }
+
+    private void flipEffects(PlayerEntity player) {
+        List<StatusEffectInstance> statusEffectsCopy = new ArrayList<>(player.getStatusEffects());
+        List<StatusEffect> effectsToRemove = new ArrayList<>();
+        boolean triggered = false;
+        for (StatusEffectInstance instance : statusEffectsCopy) {
+            StatusEffect effect = instance.getEffectType();
+            if (effect.getCategory() == StatusEffectCategory.HARMFUL) {
+                int duration = (int) (instance.getDuration() / 3f);
+                int amplifier = (int) (instance.getAmplifier() / 2f);
+                StatusEffect newEffect = StatusEffects.REGENERATION;
+                for (StatusEffect harmful : flippableEffects.keySet()) {
+                    if (effect.equals(harmful)) {
+                        newEffect = flippableEffects.get(harmful);
+                        break;
+                    }
+                }
+                effectsToRemove.add(effect);
+                triggered = true;
+                player.addStatusEffect(new StatusEffectInstance(newEffect, duration, amplifier));
+            }
+        }
+        for (StatusEffect effectToRemove : effectsToRemove) {
+            player.removeStatusEffect(effectToRemove);
+        }
+        if (triggered && !player.isCreative()) {
+            player.getItemCooldownManager().set(ItemRegistry.CHAOS_CROWN, ConfigConstructor.chaos_crown_flip_effect_cooldown);
+            player.getItemCooldownManager().set(ItemRegistry.CHAOS_HELMET, ConfigConstructor.chaos_crown_flip_effect_cooldown);
+        }
+    }
+
     private boolean isHelmetEquipped(PlayerEntity player) {
         ItemStack helmet = player.getInventory().getArmorStack(3);
         return !helmet.isEmpty() && (helmet.isOf(ItemRegistry.CHAOS_CROWN) || helmet.isOf(ItemRegistry.CHAOS_HELMET));
@@ -140,7 +190,7 @@ public class ChaosSet extends ArmorItem implements GeoItem {
         return !chest.isEmpty() && chest.isOf(ItemRegistry.ARKENPLATE) && player.getHealth() < player.getMaxHealth()/2;
     }
 
-	private PlayState predicate(AnimationState event) {
+	private PlayState predicate(AnimationState<?> event) {
         if (this == ItemRegistry.CHAOS_ROBES) {
             event.getController().setAnimation(RawAnimation.begin().then("idle", Animation.LoopType.LOOP));
         }
@@ -165,6 +215,11 @@ public class ChaosSet extends ArmorItem implements GeoItem {
                 tooltip.add(Text.translatable("tooltip.soulsweapons.chaos_crown_description_1").formatted(Formatting.GRAY));
                 tooltip.add(Text.translatable("tooltip.soulsweapons.chaos_crown_description_2").formatted(Formatting.GRAY));
                 tooltip.add(Text.translatable("tooltip.soulsweapons.chaos_crown_description_3").formatted(Formatting.GRAY));
+                tooltip.add(Text.translatable("tooltip.soulsweapons.reversal").formatted(Formatting.DARK_AQUA));
+                tooltip.add(Text.translatable("tooltip.soulsweapons.reversal_1").formatted(Formatting.GRAY));
+                tooltip.add(Text.translatable("tooltip.soulsweapons.reversal_2").formatted(Formatting.GRAY));
+                tooltip.add(Text.translatable("tooltip.soulsweapons.reversal_3").formatted(Formatting.GRAY));
+                tooltip.add(Text.translatable("tooltip.soulsweapons.reversal_4").formatted(Formatting.DARK_GRAY));
                 if (stack.isOf(ItemRegistry.CHAOS_CROWN)) {
                     if (Screen.hasControlDown()) {
                         for (int i = 1; i <= 4; i++) {
@@ -217,7 +272,7 @@ public class ChaosSet extends ArmorItem implements GeoItem {
     @Override
     public void createRenderer(Consumer<Object> consumer) {
         consumer.accept(new RenderProvider() {
-            private GeoArmorRenderer renderer;
+            private GeoArmorRenderer<?> renderer;
 
             @Override
             public BipedEntityModel<LivingEntity> getHumanoidArmorModel(LivingEntity livingEntity, ItemStack itemStack, EquipmentSlot equipmentSlot, BipedEntityModel<LivingEntity> original) {
