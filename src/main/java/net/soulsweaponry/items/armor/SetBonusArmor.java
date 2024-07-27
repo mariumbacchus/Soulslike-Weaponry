471ddb6b58288f4fabd04da1a92a7c771850ea6d
marium <mariummailtest@gmail.com>
2024-07-27 21:03:34 +0200

    Fixed ForlornArmor healing too little issue

--- src/main/java/net/soulsweaponry/items/armor/SetBonusArmor.java
+++ src/main/java/net/soulsweaponry/items/armor/SetBonusArmor.java
@@ -31,7 +31,7 @@ public abstract class SetBonusArmor extends ModdedArmor {
                 for (StatusEffectInstance instance : this.getFullSetEffects()) {
                     player.addStatusEffect(instance);
                 }
-                this.tickAdditionalSetEffects(player);
+                this.tickAdditionalSetEffects(stack, player);
             }
         }
     }
@@ -48,7 +48,7 @@ public abstract class SetBonusArmor extends ModdedArmor {
         return bootsSlot && leggingsSlot && chestSlot && helmetSlot;
     }
 
-    protected abstract void tickAdditionalSetEffects(PlayerEntity player);
+    protected abstract void tickAdditionalSetEffects(ItemStack stack, PlayerEntity player);
     protected abstract Item getMatchingBoots();
     protected abstract Item getMatchingLegs();
     protected abstract Item getMatchingChest();
