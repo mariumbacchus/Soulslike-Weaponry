e1e22e3316461dead1bd08697983ed7c6a758a04
marium <mariummailtest@gmail.com>
2024-09-20 21:14:43 +0200

    Added rest of projectiles to is_projectile.json tag - This fixes issues with other bonus damage/skill tree mods like Pufferfish not applying damage correctly to ranged weapons MoonlightArrow has permanent piercing level 4 and applies magic damage via DamageSourcesMixin instead of overriding onEntityHit Parry checks whether item is in c:shields tag or not EvilForlorn has isGlowing set to true instead of adding GLOWING status effect constantly Added runtime only mods for testing

---
+++ src/main/java/net/soulsweaponry/mixin/DamageSourcesMixin.java
@@ -0,0 +1,23 @@
+package net.soulsweaponry.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageSources;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.soulsweaponry.entity.projectile.MoonlightArrow;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DamageSources.class)
+public class DamageSourcesMixin {
+
+    @Inject(method = "arrow", at = @At("HEAD"), cancellable = true)
+    public void interceptArrowDamageSource(PersistentProjectileEntity source, Entity attacker, CallbackInfoReturnable<DamageSource> info) {
+        if (source instanceof MoonlightArrow) {
+            DamageSources sources = (DamageSources)(Object)this;
+            info.setReturnValue(sources.indirectMagic(source, attacker));
+        }
+    }
+}
