7e89cf937eae921559e189d593b5bbc266f87fec
mariuj0909 <mariuj0909@osloskolen.no>
2024-09-30 22:28:52 +0200

    Added new kraken slayer models Added 2 new built-in resourcepacks containing old kraken slayer models and textures Added config line for use action animation for kraken slayer bow Renamed Kraken Slayer Bow to Kraken Slayer Spear Bow

--- src/main/java/net/soulsweaponry/SoulsWeaponry.java
+++ src/main/java/net/soulsweaponry/SoulsWeaponry.java
@@ -60,6 +60,14 @@ public class SoulsWeaponry implements ModInitializer {
             ResourceManagerHelper.registerBuiltinResourcePack(new Identifier(ModId, "2d_weapons"), modContainer, "2D Weapon Models", ResourcePackActivationType.NORMAL);
             LOGGER.info("Successfully registered built-in 2D model resourcepack!");
         });
+        FabricLoader.getInstance().getModContainer(ModId).ifPresent(modContainer -> {
+            ResourceManagerHelper.registerBuiltinResourcePack(new Identifier(ModId, "old_kraken_slayer_2d"), modContainer, "Old 2D Kraken Slayer Model", ResourcePackActivationType.NORMAL);
+            LOGGER.info("Successfully registered built-in Old 2D Kraken Slayer Model resourcepack!");
+        });
+        FabricLoader.getInstance().getModContainer(ModId).ifPresent(modContainer -> {
+            ResourceManagerHelper.registerBuiltinResourcePack(new Identifier(ModId, "old_kraken_slayer_3d"), modContainer, "Old 3D Kraken Slayer Model", ResourcePackActivationType.NORMAL);
+            LOGGER.info("Successfully registered built-in Old 3D Kraken Slayer Model resourcepack!");
+        });
 
         long end = System.currentTimeMillis();
         LOGGER.info("Initializing done, time taken: " + (end - start) + "ms");
