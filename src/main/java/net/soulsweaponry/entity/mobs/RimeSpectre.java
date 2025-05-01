43d39b1d77ae875fcd31b65dfa1e991bfad44b86
marium <mariumbacchus@gmail.com>
2025-05-01 10:40:11 +0200

    RimeSpectre can't have FREEZING effect applied

--- src/main/java/net/soulsweaponry/entity/mobs/RimeSpectre.java
+++ src/main/java/net/soulsweaponry/entity/mobs/RimeSpectre.java
@@ -30,6 +30,7 @@ import net.soulsweaponry.registry.EffectRegistry;
 import net.soulsweaponry.util.IAnimatedDeath;
 import net.soulsweaponry.particles.ParticleEvents;
 import net.soulsweaponry.particles.ParticleHandler;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
@@ -209,6 +210,14 @@ public class RimeSpectre extends Remnant implements GeoEntity, IAnimatedDeath {
             }
         }
         if (this.hasStatusEffect(EffectRegistry.FREEZING.get())) this.removeStatusEffect(EffectRegistry.FREEZING.get());
+    }
+
+    @Override
+    public boolean addStatusEffect(StatusEffectInstance effect, @Nullable Entity source) {
+        if (effect.getEffectType().equals(EffectRegistry.FREEZING.get())) {
+            return false;
+        }
+        return super.addStatusEffect(effect, source);
     }
 
     @Override
